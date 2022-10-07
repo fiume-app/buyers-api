c33807f961cab7fe8b1e330170f1b049a5e816e9
Vinayak Nivalkar <57988280+VinayakNivalkar@users.noreply.github.com>
2022-10-07 13:31:22 +0530

    routing updated

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,4 +1,5 @@
 import { FastifyPluginAsync } from 'fastify';
+import { fetch_buyer } from './plugins/fetch_buyer';
 import { verify_firebase_id_token } from './plugins/verify_firebase_id_token';
 
 import * as index from './routes';
@@ -18,5 +19,26 @@ export const v1: FastifyPluginAsync = async (instance, _opts) => {
     method: 'POST',
     preHandler: verify_firebase_id_token,
     handler: index.POST_handler,
+  });
+
+  instance.route({
+    url: '/addresses',
+    method: 'GET',
+    preHandler: [
+      verify_firebase_id_token,
+      fetch_buyer,
+    ],
+    handler: index.addresses.GET_handler,
+  });
+
+  instance.route({
+    url: '/addresses',
+    method: 'POST',
+    schema: index.addresses.POST_validation_schema,
+    preHandler: [
+      verify_firebase_id_token,
+      fetch_buyer,
+    ],
+    handler: index.addresses.POST_handler,
   });
 };
